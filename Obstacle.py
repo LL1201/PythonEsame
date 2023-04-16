3c69e097085c5d8579c2b5719003068dceec6991
LL1201 <luca.loner04@gmail.com>
2023-04-16 11:40:47 +0200

    Aggiunti ostacoli randomici

--- Obstacle.py
+++ Obstacle.py
@@ -1,23 +1,54 @@
 import pygame
 
 
-class Obstacle(object):
-    def __init__(self, posx, posy, obstacleImage):
-        self.image = pygame.transform.scale(
-            obstacleImage, (obstacleImage.get_rect().width/6, obstacleImage.get_rect().height/6))
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, posx, posy):
+        super().__init__()
+        self.sprites = []
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\FB500-1.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\FB500-2.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\FB500-3.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\FB500-4.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\FB500-5.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\B500-2.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\B500-3.png"))
+        self.sprites.append(pygame.image.load(
+            "images\\assets\\fireball\\B500-4.png"))
+        self.currentSprite = 0
+        self.image = self.sprites[self.currentSprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [posx, posy]
 
         self.w, self.h = pygame.display.get_surface().get_size()
         self.x = posx
         self.y = posy
-
-    def changePosition(self, newx, newy):
-        self.x = newx
-        self.y = newy
+        self.explodes = False
 
     def move(self):
-        self.y += 2
-        if self.y > self.h:
-            self.changePosition(0, 100)
+        self.y += 4
+        self.rect.topleft = [self.x, self.y]
+        if self.y == 400:
+            self.explodes = True
+            self.currentSprite = 5
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
+
+    def update(self):
+        self.move()
+        self.currentSprite += 0.5
+
+        if self.currentSprite >= 4 and not self.explodes:
+            self.currentSprite = 0
+        elif self.currentSprite >= 7 and self.explodes:
+            self.kill()
+
+        self.image = self.sprites[int(self.currentSprite)]
--- game.py
+++ game.py
@@ -5,22 +5,47 @@ import Player
 import Obstacle
 
 playerImage = pygame.image.load(os.path.join('images', 'player.png'))
-obstacleImage = pygame.image.load(os.path.join('images', 'putin.png'))
 clock = pygame.time.Clock()
 running = True
 
 
-def randomObstacle():
-    return random.randint(0, 2)
+def randomObstacleProperties():
+    n = random.randint(1, 2)
+    posx = []
+
+    for _ in range(n):
+        posx.append(random.randint(-25, 250))
+
+    return {"n": n, "posx": posx}
+
+
+def generateObstacles():
+    properties = randomObstacleProperties()
+    obstaclesList = []
+    for i in range(properties["n"]):
+        obstaclesList.append(Obstacle.Obstacle(properties["posx"][i], -100))
+
+    return obstaclesList
 
 
 pygame.init()
-screen = pygame.display.set_mode((400, 600))
 
+# screen
+screenW = 400
+screenH = 600
+screen = pygame.display.set_mode((screenW, screenH))
+
+# sprites
 player = Player.Player(playerImage)
-obstacles = [Obstacle.Obstacle(0, -100, obstacleImage),
-             Obstacle.Obstacle(200, -100, obstacleImage), Obstacle.Obstacle(300, -100, obstacleImage)]
-rand = randomObstacle()
+obstacles = pygame.sprite.Group()
+# obstacle = Obstacle.Obstacle(-25, -100)
+# obstacle1 = Obstacle.Obstacle(110, -100)
+# obstacle2 = Obstacle.Obstacle(250, -100)
+# obstacles.add(obstacle)
+# obstacles.add(obstacle1)
+# obstacles.add(obstacle2)
+# rand = randomObstacle()
+newObstaclesCount = 1
 
 while running:
     for event in pygame.event.get():
@@ -30,12 +55,17 @@ while running:
 
     if not running:
         break
+
+    if int(newObstaclesCount) != 0:
+        obstacles.add(generateObstacles())
+        newObstaclesCount = 0
+    newObstaclesCount += 0.02
     player.handle_keys()
-    screen.blit(pygame.image.load(os.path.join(
-        'images', 'background.jpg')), (0, 0))
+    # screen.blit(pygame.image.load(os.path.join('images', 'background.jpg')), (0, 0))
+    screen.fill((255, 255, 255))
     player.draw(screen)
-    obstacles[rand].draw(screen)
-    obstacles[rand].move()
+    obstacles.draw(screen)
+    obstacles.update()
     pygame.display.update()
 
     clock.tick(40)
